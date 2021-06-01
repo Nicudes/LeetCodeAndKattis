11f0d915923c794f4df35f3f440a191d9eb0955c
Huy Tran <Nicudes@gmail.com>
2021-06-01 12:24:48 +0200

    Palindrom number FInished

--- CodePractice/Program.cs
+++ CodePractice/Program.cs
@@ -8,7 +8,7 @@ namespace CodePractice
     {
         static void Main(string[] args)
         {
-            Tests.TwoSum.RunTest();
+            Tests.PalindromeNumber.RunTest();
         }
 
 	
---
+++ CodePractice/Tests/PalindromeNumber.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePractice.Tests
+{
+    class PalindromeNumber
+    {
+
+        public static bool IsPalindrome(int x)
+        {
+            List<int> myIntList = new List<int>();
+            string myNumberInString = x.ToString();
+            string reversedNum = "";
+
+            if (x < 0)
+            {
+                return false;
+            }
+
+
+            myIntList = myNumberInString.Select(x => Convert.ToInt32(x.ToString())).ToList();
+            myIntList.Reverse();
+
+            foreach (var i in myIntList)
+            {
+                reversedNum += i.ToString();
+            }
+
+
+         
+            if (reversedNum == x.ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+
+        public static void RunTest()
+        {
+            bool result = IsPalindrome(404);
+            if (result)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            result = IsPalindrome(-404);
+            if (result)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            result = IsPalindrome(1234567899);
+            if (result)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+
+        }
+
+    }
+}
