05f8fb3d42293a45113c1c77b7a296dcf6e40127
Huy Tran <Nicudes@gmail.com>
2021-07-04 23:02:24 +0200

    Revert "Added magicNumbah"
    This reverts commit db205f4103d13cdd840bb1b5815c291b6d28e0ac.

--- CodePractice/Program.cs
+++ CodePractice/Program.cs
@@ -8,7 +8,7 @@ namespace CodePractice
     {
         static void Main(string[] args)
         {
-            Tests.MagicNumber.Solution();
+            Tests.GetMiddleSubstring.RunTest();
         }
 
 	
